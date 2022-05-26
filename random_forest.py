d28a5ea23336eadfb281b376f863edec0bada309
Hikari Ishihara <e205217@ie.u-ryukyu.ac.jp>
2022-05-26 16:03:02 +0900

    ランダムフォレスト_2

--- random_forest.py
+++ random_forest.py
@@ -4,8 +4,26 @@ import dataset
 from sklearn.model_selection import train_test_split, GridSearchCV #データ分割用,グリッドサーチ
 from sklearn.ensemble import RandomForestClassifier #ランダムフォレスト
 
-x_train, x_test, y_train, y_test = train_test_split(x.values, y.values,test_size = 0.3, random_state = 3)
+x_train, x_test, y_train, y_test = dataset.get_dataset()
 
+parameters = {
+    'n_estimators':[10,20,30,50,100,300],           #用意する決定木
+    'max_features':('sqrt', 'log2','auto', None),   #ランダムに指定する特徴量の数
+    'max_depth':(10,20,30,40,50,None),              #決定木のノード深さの制限
+    
+}
  
+#モデルインスタンス
+model  = RandomForestClassifier()
 
+#ハイパーパラメーターチューニング（グリッドサーチのコンストラクタにモデルと辞書パラメータを指定)
+gridsearch = GridSearchCV(estimator = model,         #モデル
+                          param_grid = parameters,   #チューニングするハイパーパラメータ
+                          scoring = "accuracy")      #スコアリング
 
+#演算実行
+gridsearch.fit(x_train, y_train)
+
+# グリッドサーチの結果から得られた最適なパラメータ候補を確認
+print('Best params: {}'.format(gridsearch.best_params_)) 
+print('Best Score: {}'.format(gridsearch.best_score_))
