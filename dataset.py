88bddac6625f2475c55d62a3819ed7ff59502064
Yabie-5 <e205744@ie.u-ryukyu.ac.jp>
2022-05-26 16:08:03 +0900

    read folder

--- dataset.py
+++ dataset.py
@@ -1,1 +1,10 @@
+from operator import le
+import os
 import pandas as pd
+
+
+path = "arc-ukiyoe-faces-main/scratch/arc_images"
+# path内のデイレクトリ下にあるフォルダ名をリストで取得。
+folders = os.listdir(path)
+
+print(len(folders))
