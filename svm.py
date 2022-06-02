e8d36cbd88e31102550ced2e64597ea81eb19814
仲宗根悠太 <nakasoneyuuta@yue.local>
2022-06-02 15:34:04 +0900

    code fix

--- svm.py
+++ svm.py
@@ -1,5 +1,5 @@
 from sklearn import svm
-from sklearn import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score
 import pandas as pd
